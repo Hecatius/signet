ee42afa929dd0e165b71fb55e330eeab819906d0
Jonathan Mores <jonathanmopro@gmail.com>
2022-03-16 15:02:52 -0400

    Premier commit, initialisation Firebase

---
+++ code/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDPQGTBouzbX5BAvUccymvJMCSplUDq1cU",
+    authDomain: "cours-react-2022.firebaseapp.com",
+    projectId: "cours-react-2022",
+    storageBucket: "cours-react-2022.appspot.com",
+    messagingSenderId: "1097206577844",
+    appId: "1:1097206577844:web:7e3a536b5ec420620b75e1",
+    measurementId: "G-Q1WZNY040V"
+  };
+
+  export default firebaseConfig;
